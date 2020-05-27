c4ac56fbde91390735fc84df52e7e4be845094ee
Matthew Leite <mattleite@comcast.net>
2020-05-27 14:17:16 -0700

    added store

---
+++ breton/src/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    cards: [],
+    page: null,
+    loading: true,
+    pageSize: 20
+  },
+  mutations: {
+    updateCards (state, cards) {
+      state.cards = cards
+    },
+    changeLoadingState (state, loading) {
+      state.loading = loading
+    },
+    updatePageCount (state) {
+      state.page++
+    }
+  }
+})
